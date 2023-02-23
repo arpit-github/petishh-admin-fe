c577da8efa266c7e7b1d896af185671c54ee9ad0
Arpit <guptaarpit09@yahoo.com>
2023-02-23 22:25:48 +0530

    middleware fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import { useEffect, useState } from "react";
 import { ConfigProvider } from "antd";
+import { useRouter } from "next/router";
 
 import "src/styles/globals.css";
 import "src/styles/variables.css";
 import "src/styles/antd.css";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
   const [themeColors, setThemeColors] = useState({});
 
   useEffect(() => {
@@ -16,8 +18,14 @@ export default function App({ Component, pageProps }: AppProps) {
         document.documentElement
       ).getPropertyValue("--primary-color");
       setThemeColors({ colorPrimary });
+      const token = localStorage["user-access-token"];
+      if (token && router.pathname === "/login") {
+        router.replace("/dashboard");
+      } else if (!token && router.pathname !== "/login") {
+        router.replace("/login");
+      }
     }
-  }, []);
+  }, [router]);
 
   return (
     <>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,9 +8,8 @@ const Home = () => {
     const isLoggedIn = !!(
       typeof window !== "undefined" && localStorage.getItem("user-access-token")
     );
-
     router.replace(isLoggedIn ? "/dashboard" : "/login");
-  }, [router]);
+  }, []);
 
   return <div style={{ padding: "2rem" }}>Redirecting...</div>;
 };
